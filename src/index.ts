52bf7fc00cd1435781faefe206bf516dc0c27c2a
vijayparmar27 <vijayparmar0027@gmail.com>
2024-07-17 19:27:10 +0530

    add code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 // src/index.ts
 
-export interface MyInterface {
-  name: string;
-  age: number;
-}
-
 export function greet(person: MyInterface): string {
   return `Hello, ${person.name}! You are ${person.age} years old.`;
+}
+
+import mongoose from "mongoose";
+import { MyInterface } from "./interface/app";
+
+export const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+});
+
+export class MongoSchema {
+  private mongoose: any;
+
+  constructor(mongoose: any) {
+    this.mongoose = mongoose;
+  }
+
+  get userSchema() {
+    return new this.mongoose.Schema({
+      name: {
+        type: String,
+      },
+    });
+  }
 }
